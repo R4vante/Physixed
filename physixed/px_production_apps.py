21616f3a825781d6b4da5f2e40757c47b9ad8012
R4vante <l.teegelbeckers@hotmail.com>
2024-05-20 23:10:26 +0200

    fix: configure settings

---
+++ physixed/px_production_apps.py
@@ -0,0 +1,11 @@
+from typing import Any
+
+baseline_apps: dict[str, Any] = {
+    ## Baseline apps: run always
+    ############################
+}
+
+production_apps: dict[str, Any] = {
+    ## Production apps
+    ##################
+}
