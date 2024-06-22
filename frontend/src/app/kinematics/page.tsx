e086780db1b42e8559f9cf0660911ec7db0e6eea
R4vante <l.teegelbeckers@hotmail.com>
2024-06-22 16:09:46 +0200

    refactor: create plotly graph

---
+++ frontend/src/app/kinematics/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+import React from "react";
+
+const KinematicsHome = () => {
+  return <Link href="/kinematics/freefall">Free fall</Link>;
+};
+
+export default KinematicsHome;
