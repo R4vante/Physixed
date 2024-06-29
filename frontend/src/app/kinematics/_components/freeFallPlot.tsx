4c5be8293f64c74ee6e596e02ef0b7feef7985ef
R4vante <l.teegelbeckers@hotmail.com>
2024-06-30 00:22:50 +0200

    refactor(kinematics): :recycle: simplify plot functionality by seperate layout and data in functions

--- frontend/src/app/kinematics/_components/freeFallPlot.tsx
+++ frontend/src/app/kinematics/_components/freeFallPlot.tsx
@@ -4,37 +4,33 @@ import { cn } from "@/lib/utils";
 import dynamic from "next/dynamic";
 import React from "react";
 import { useTheme } from "next-themes";
-import { Data } from "plotly.js";
+import { Data, Layout } from "plotly.js";
 
 const Plot = dynamic(() => import("react-plotly.js"), { ssr: false });
 
-const FreeFallPlot = (plotData: GraphProps, className: string) => {
-  const { resolvedTheme } = useTheme();
+const adjustLayout = (layout: Partial<Layout>, isDark: boolean) => ({
+  ...layout,
+  autosize: true,
+  paper_bgcolor: "rgba(0,0,0,0)",
+  plot_bgcolor: isDark ? "#25272d" : "f0f0f0",
+  font: {
+    color: isDark ? "#c6d6e7" : "black",
+  },
+  xaxis: {
+    ...layout.xaxis,
+    gridcolor: isDark ? "#c6d6e7" : "#ddd",
+    zerolinecolor: isDark ? "#c6d6e7" : "#ddd",
+  },
+  yaxis: {
+    ...layout.yaxis,
+    gridcolor: isDark ? "#c6d6e7" : "#ddd",
+    zerolinecolor: isDark ? "#c6d6e7" : "#ddd",
+    range: layout.yaxis?.range || [0, null],
+  },
+});
 
-  const isDark = resolvedTheme === "dark";
-
-  const adjustedLayout = {
-    ...plotData.layout,
-    autosize: true,
-    paper_bgcolor: "rgba(0,0,0,0)",
-    plot_bgcolor: isDark ? "#25272d" : "f0f0f0",
-    font: {
-      color: isDark ? "#c6d6e7" : "black",
-    },
-    xaxis: {
-      title: plotData.layout.xaxis?.title || {},
-      gridcolor: isDark ? "#c6d6e7" : "#ddd",
-      zerolinecolor: isDark ? "#c6d6e7" : "#ddd",
-    },
-    yaxis: {
-      title: plotData.layout.yaxis?.title || {},
-      gridcolor: isDark ? "#c6d6e7" : "#ddd",
-      zerolinecolor: isDark ? "#c6d6e7" : "#ddd",
-      range: plotData.layout.yaxis?.range || [0, null],
-    },
-  };
-
-  const adjustedData: Partial<Data>[] = plotData.data.map((trace) => ({
+const adjustData = (data: Partial<Data>[], isDark: boolean): Partial<Data>[] =>
+  data.map((trace) => ({
     ...trace,
     line: {
       color: isDark ? "#4be" : "#45e",
@@ -42,6 +38,14 @@ const FreeFallPlot = (plotData: GraphProps, className: string) => {
       dash: "solid",
     },
   }));
+
+const FreeFallPlot = (plotData: GraphProps, className: string) => {
+  const { resolvedTheme } = useTheme();
+
+  const isDark = resolvedTheme === "dark";
+
+  const adjustedLayout = adjustLayout(plotData.layout, isDark);
+  const adjustedData = adjustData(plotData.data, isDark);
 
   return (
     <div className={cn("h-[450px]", className)}>
