c6c8de62bd60ffe9a356c9897fa83358b777c50a
R4vante <l.teegelbeckers@hotmail.com>
2024-06-29 18:55:48 +0200

    refactor(kinematics): :recycle: Remove commented code in lineplot

--- backend/kinematics/pytools/lineplot.py
+++ backend/kinematics/pytools/lineplot.py
@@ -42,10 +42,7 @@ def make_plot(x_data: pint.Quantity, y_data: pint.Quantity) -> str:
         xaxis_title=f"{compact_unit_x:P}",
         yaxis_title=f"{compact_unit_y:P}",
         modebar={"orientation": "v"},
-        # paper_bgcolor="rgba(0,0,0,0)",
         margin={"l": 5, "r": 5, "t": 5, "b": 5},
-        # width=400,
-        # height=400,
     )
 
     return fig.to_json()
