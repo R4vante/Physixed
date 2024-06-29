1f428f5a84014699fa19f07b22331e2e859c226d
R4vante <l.teegelbeckers@hotmail.com>
2024-06-29 17:21:48 +0200

    style(kinematics): :lipstick: Change color plot according to theme

--- frontend/src/app/kinematics/_components/freeFallPlot.tsx
+++ frontend/src/app/kinematics/_components/freeFallPlot.tsx
@@ -1,31 +1,49 @@
+"use client";
 import { GraphProps } from "@/lib/types";
 import { cn } from "@/lib/utils";
 import dynamic from "next/dynamic";
 import React from "react";
+import { useTheme } from "next-themes";
+import { Data } from "plotly.js";
 
 const Plot = dynamic(() => import("react-plotly.js"), { ssr: false });
 
 const FreeFallPlot = (plotData: GraphProps, className: string) => {
+  const { resolvedTheme } = useTheme();
+
+  const isDark = resolvedTheme === "dark";
+
   const adjustedLayout = {
     ...plotData.layout,
     autosize: true,
-    margin: {
-      l: 50, // Linker marge
-      r: 10, // Rechter marge, zo klein mogelijk
-      b: 50, // Onderste marge
-      t: 50, // Bovenste marge
-      pad: 0, // Padding
+    paper_bgcolor: "rgba(0,0,0,0)",
+    plot_bgcolor: isDark ? "#25272d" : "f0f0f0",
+    font: {
+      color: isDark ? "#c6d6e7" : "black",
+    },
+    xaxis: {
+      gridcolor: isDark ? "#c6d6e7" : "#ddd",
+      zerolinecolor: isDark ? "#c6d6e7" : "#ddd",
+    },
+    yaxis: {
+      gridcolor: isDark ? "#c6d6e7" : "#ddd",
+      zerolinecolor: isDark ? "#c6d6e7" : "#ddd",
     },
   };
+
+  const adjustedData: Partial<Data>[] = plotData.data.map((trace) => ({
+    ...trace,
+    line: {
+      color: isDark ? "#4be" : "#45e",
+      width: 2,
+      dash: "solid",
+    },
+  }));
+
   return (
-    <div
-      className={cn(
-        "",
-        className
-      )}
-    >
+    <div className={cn("h-[450px]", className)}>
       <Plot
-        data={plotData.data}
+        data={adjustedData}
         layout={adjustedLayout}
         useResizeHandler
         style={{ width: "100%", height: "100%" }}
