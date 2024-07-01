fa39e77aa9e30137b3cc51a8a69770541ee91028
R4vante <l.teegelbeckers@hotmail.com>
2024-07-01 02:00:05 +0200

    style(home): :lipstick: Add tekst to hero section

--- frontend/src/app/(home)/_components/Hero.tsx
+++ frontend/src/app/(home)/_components/Hero.tsx
@@ -1,18 +1,32 @@
 import HeroCards from "@/app/(home)/_components/HeroCards";
 import "../index.css";
+import Image from "next/image";
+import BlackHoleImg from "@/assets/blackhole.jpeg";
 
 const Hero = () => {
   return (
     <>
-      <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
+      <section className="relative container grid lg:grid-cols-2 place-items-center py-20 md:py-16 gap-10">
         <div className="text-center lg:text-start space-y-6">
           <section className="text-5xl md:text-6xl font-bold">
-            <h1 className="inline">Here comes some text</h1>
+            <h1 className="inline text-5xl md:text-6xl">
+              Discover{" "}
+              <span className="inline bg-gradient-to-r from-[#96eaf5]  to-[#47cdd2] text-transparent bg-clip-text">
+                physics
+              </span>{" "}
+              in an interactive way with{" "}
+              <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
+                Physixed
+              </span>
+              !
+            </h1>
           </section>
+          <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
+            Explore intuitive models and simulations that make learning physics
+            exciting and easy.
+          </p>
         </div>
-        <div className="z-10">
-          <HeroCards />
-        </div>
+
         <div className="animation-bg"></div>
       </section>
     </>
