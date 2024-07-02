15de74cfda6809e9e67ac9f49b4f22e2a8c317d5
R4vante <l.teegelbeckers@hotmail.com>
2024-07-02 22:01:35 +0200

    style(home): :lipstick: update hero stection for big screens

--- frontend/src/app/(home)/_components/Hero.tsx
+++ frontend/src/app/(home)/_components/Hero.tsx
@@ -5,7 +5,7 @@ import HeroImage from "@/app/(home)/_components/HeroImage";
 const Hero = () => {
   return (
     <>
-      <section className="relative h-[100vh] container grid lg:grid-cols-2 place-items-center py-20 md:py-16 lg:mb-12 gap-10">
+      <section className="relative h-[100vh] container grid lg:grid-cols-2 place-items-center py-20 md:py-16 lg:mb-12 xl:w-full gap-10">
         <div className="text-center lg:text-start space-y-6">
           <section className="text-5xl md:text-6xl font-bold">
             <h1 className="inline text-5xl md:text-6xl">
@@ -26,7 +26,7 @@ const Hero = () => {
           </p>
         </div>
 
-        <div className="animation-bg"></div>
+        <div className="animation-bg justify-self-end"></div>
         <HeroImage
           className="hidden lg:flex absolute -right-[3rem] -bottom-px"
           width={700}
