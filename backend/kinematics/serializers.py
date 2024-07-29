8ae87cc1f92fc5072675f46a573cb69a77eb80c3
R4vante <l.teegelbeckers@hotmail.com>
2024-07-30 00:21:20 +0200

    feat(air_res): :triangular_flag_on_post: add serializer for air resistance model

--- backend/kinematics/serializers.py
+++ backend/kinematics/serializers.py
@@ -13,3 +13,24 @@ class FreeFallSerializer(serializers.Serializer):
     height_unit = serializers.ChoiceField(choices=[("m", "m"), ("km", "km")])
     velocity = serializers.FloatField()
     velocity_unit = serializers.ChoiceField(choices=[("m/s", "m/s"), ("km/h", "km/h")])
+
+
+class AirResistanceSerializer(serializers.Serializer):
+    """Serializer for the air resistance model.
+
+    Args
+        serializers (Serializers): Serializer class for the air resistance model.
+
+    """
+
+    height = serializers.FloatField()
+    height_unit = serializers.ChoiceField(choices=[("m", "m"), ("km", "km")])
+    velocity = serializers.FloatField()
+    velocity_unit = serializers.ChoiceField(choices=[("m/s", "m/s"), ("km/h", "km/h")])
+    drag_coefficient = serializers.FloatField()
+    cross_sectional_area = serializers.FloatField()
+    cross_sectional_area_unit = serializers.ChoiceField(choices=[("m^2", "m^2"), ("cm^2", "cm^2")])
+    mass = serializers.FloatField()
+    mass_unit = serializers.ChoiceField(choices=[("kg", "kg"), ("g", "g")])
+    time = serializers.FloatField()
+    time_unit = serializers.ChoiceField(choices=[("s", "s"), ("min", "min")])
