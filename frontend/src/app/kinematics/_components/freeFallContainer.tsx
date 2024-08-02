6910b1166cac47dfb1586ca0ec71a9a691d39852
R4vante <l.teegelbeckers@hotmail.com>
2024-08-02 17:57:01 +0200

    fix(kinematics): remove the cardflip on plot change to make sure plot updates on form submit

--- frontend/src/app/kinematics/_components/freeFallContainer.tsx
+++ frontend/src/app/kinematics/_components/freeFallContainer.tsx
@@ -1,7 +1,7 @@
 "use client";
 import FreeFallForm from "@/app/kinematics/_components/freeFallForm";
 import { GraphProps, TFreeFall } from "@/lib/types";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import CardWrapper from "@/components/ui/cardWrapper";
 import toast from "react-hot-toast";
@@ -37,7 +37,6 @@ const FreeFallContainer = () => {
       }
 
       const parsedHeightData: GraphProps = JSON.parse(result.height_dict);
-
       const parsedVelocityData: GraphProps = JSON.parse(result.velocity_dict);
 
       if (!parsedHeightData.data || !parsedHeightData.layout) {
@@ -66,45 +65,21 @@ const FreeFallContainer = () => {
 
       <div className="flex flex-col items-center w-full lg:w-1/2">
         {heightData && velocityData && (
-          <AnimatePresence mode="wait">
-            {isVelocity ? (
-              <motion.div
-                key={isVelocity ? "velocity" : "height"}
-                className="mt-4 w-full h-full flex-grow"
-                initial={{ opacity: 0, rotateY: 90 }}
-                animate={{ opacity: 1, rotateY: 0 }}
-                exit={{ opacity: 0, rotateY: -90 }}
-                transition={{
-                  duration: 0.5,
-                }}
-              >
-                <Plot
-                  data={velocityData}
-                  isVelocity={isVelocity}
-                  setIsVelocity={setIsVelocity}
-                  buttonTitle="Plot Velocity"
-                />
-              </motion.div>
-            ) : (
-              <motion.div
-                key={isVelocity ? "velocity" : "height"}
-                className="mt-4 w-full h-full flex-grow"
-                initial={{ opacity: 0, rotateY: 90 }}
-                animate={{ opacity: 1, rotateY: 0 }}
-                exit={{ opacity: 0, rotateY: -90 }}
-                transition={{
-                  duration: 0.5,
-                }}
-              >
-                <Plot
-                  data={heightData}
-                  isVelocity={isVelocity}
-                  setIsVelocity={setIsVelocity}
-                  buttonTitle="Plot Velocity"
-                />
-              </motion.div>
-            )}
-          </AnimatePresence>
+          <motion.div
+            className="mt-4 w-full h-full flex-grow"
+            initial={{ opacity: 0, rotateY: 90 }}
+            animate={{ opacity: 1, rotateY: 0 }}
+            transition={{
+              duration: 0.5,
+            }}
+          >
+            <Plot
+              data={isVelocity ? velocityData : heightData}
+              isVelocity={isVelocity}
+              setIsVelocity={setIsVelocity}
+              buttonTitle="Plot Velocity"
+            />
+          </motion.div>
         )}
       </div>
     </div>
