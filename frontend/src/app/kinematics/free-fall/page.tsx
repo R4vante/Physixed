c6e83f6d2572cdf15b44f6d0f7c64bc470d899da
R4vante <l.teegelbeckers@hotmail.com>
2024-08-06 20:14:54 +0200

    chore: :truck: rename free fall page

---
+++ frontend/src/app/kinematics/free-fall/page.tsx
@@ -0,0 +1,19 @@
+import dynamic from "next/dynamic";
+import React from "react";
+
+const FreeFallContainer = dynamic(
+  () => import("@/app/kinematics/free-fall/_components/freeFallContainer"),
+  { ssr: false }
+);
+
+const FreeFall = () => {
+  return (
+    <>
+      <section className="flex w-full h-auto flex-col pt-15 mt-[56px] items-center">
+        <FreeFallContainer />
+      </section>
+    </>
+  );
+};
+
+export default FreeFall;
